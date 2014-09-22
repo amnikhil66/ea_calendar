7e681b89dd113983d98af5f489e1daf66ee3be4a
phoenix0665 <phoenix0665@github.com>
2014-09-22 08:45:57 +0530

    Included countdown-timer logic.

--- js/ea_calendar.js
+++ js/ea_calendar.js
@@ -9,16 +9,78 @@
             $calendarHeadRow = null,
             $calendar = $("<table></table>");
 
+        var getDate = function(time_string){
+            var date = new Date(),
+                time = time_string.replace(/:/g, "");
+
+            date.setHours(parseInt(time.substring(0,2)),
+                    parseInt(time.substring(2,4)),
+                    parseInt(time.substring(4,6)));
+
+            return date;
+        };
+
+        var addDetailedView = function(date){
+            var now = new Date(),
+                time_difference = ((date - now) / (1000 * 60)).toFixed(2),
+                mins = time_difference >= 10 ? time_difference : "0" + Math.floor(time_difference),
+                secs = Math.floor((time_difference % 1) * 60),
+                triggerInterval = null;
+
+                secs = secs >= 10 ? secs : "0" + secs;
+
+            if(time_difference >= 0 && time_difference <= 15){
+                var $detailsRow = $calendarBody.append("<tr>").find("tr:last-child"),
+                    counter = (time_difference >= 15) ? "14:59" : mins + ":" + secs;
+
+                $detailsRow.append("<td class='countdown_timer'>"
+                    + counter + "</td>");
+
+                var appendNewTime = function(){
+                    var $timer = $detailsRow.find(".countdown_timer"),
+                        time = $timer.text(),
+                        mins = parseInt(time.match(/^\d*/)[0]),
+                        secs = parseInt(time.match(/\d*$/)[0]) - 1;
+
+                    debugger;
+
+                    if(secs === 0){
+                        mins != 0 ? mins-- : mins;
+
+                        if(mins === 0){
+                            clearInterval(triggerInterval);
+                        }else{
+                            secs = 59;
+                        };
+                    };
+
+                    if(mins < 10){
+                        mins = "0" + mins;
+                    };
+
+                    if(secs < 10){
+                        secs = "0" + secs;
+                    };
+
+                    
+
+                    $timer.text(mins + ":" + secs)
+                };
+
+                triggerInterval = setInterval(appendNewTime, 1000);
+            };
+        };
+
         var loadEvents = function(data){
             events = data;
             $calendarBody = $calendar.append("<tbody>").find("tbody");
 
             $.each(events, function(index, event){
                 var $headers = $calendarHeadRow.find("th"),
-                    $thisRow = $calendarBody.append("<tr>").find("tr:last-child");
+                    $thisRow = $calendarBody.append("<tr>").find("tr:last-child"),
+                    date = getDate(event["Time"]);
 
                 $.each($headers, function(index, header){
-                    debugger;
                     var key = header.textContent.trim();
 
                     if(event.hasOwnProperty(key)){
@@ -27,6 +89,8 @@
                         $thisRow.append("<td></td>");
                     };
                 });
+
+                addDetailedView(date);
             });
 
             __this.append($calendar);
